115e7710327cf601024e2c82f8d34c9e55689bbc
arhamshafi <arhambutt2923@gmail.com>
2025-12-13 00:08:03 +0500

    added action files

--- src/app/(pages)/(public)/todos/action.js
+++ src/app/(pages)/(public)/todos/action.js
@@ -5,21 +5,19 @@ import TODO from "@/model/todo"
 import { getServerSession } from "next-auth"
 
 
-const session = getServerSession()
-console.log(session);
 
 export const addTodo = async (obj) => {
-    console.log(obj);
-    
-    try {
-        const title = await obj.get("title")
-        if (!title) throw new Error("Field Must Be Required !")
-        await ConnectDB()
-        
 
-        const newTodo = await TODO.create({ title: title })
-        return { success: true, message: "Successfully Created" }
+
+    try {
+        const session = await getServerSession()
+        console.log(session);
+        if(!session) throw new Error("Authentication Required")
+        if(obj == "") throw new Error("not Filled")
+        await ConnectDB()
+        const newTodo = await TODO.create({ title: obj })
+        return JSON.stringify({ success: true, message: "Successfully Created" })
     } catch (err) {
-        return { success: false, message: err.message || "Invalid Error" }
+        return JSON.stringify({ success: false, message: err.message || "Invalid Error" })
     }
 }
--- src/components/TodoInput.jsx
+++ src/components/TodoInput.jsx
@@ -1,36 +1,40 @@
 "use client"
 
 import { addTodo } from '@/app/(pages)/(public)/todos/action'
-import React, { useActionState, useEffect, useReducer, useRef, useState } from 'react'
+import React, { useReducer, useRef, useState } from 'react'
 import { toast } from 'react-toastify'
 
 export default function TodoInput() {
 
+    const [todo, settodo] = useState("")
     const [loader, setloader] = useState(false)
     const inpRef = useRef(null)
-    const [state, formAction] = useActionState(addTodo, { success: null, message: null })
 
-    useEffect(() => {
-        if (state.success !== null) {
-            setloader(false)
-            if (state.success) {
-                toast.success(state.message)
-                inpRef.current.value = ""
-            } else {
-                toast.error(state.message)
-                inpRef.current.focus()
+    const Submit = async (e) => {
+        e.preventDefault()
+        try {
+            setloader(true)
+            const res = await addTodo(todo)
+            const data = JSON.parse(res)
+            console.log(data);
+
+            if (!data.success) throw new Error(data.message)
+            if (data.success) {
+                toast.success(data.message)
+                settodo("")
             }
-        }
 
-    }, [state])
+        } catch (err) {
+            toast.error(err.message || "Error In Todos")
+        } finally {
+            setloader(false)
+        }
+    }
 
     return (
 
-        <form action={(obj) => {
-            setloader(true)
-            return formAction(obj)
-        }} className='flex justify-between items-center w-[800px] mt-10  ' >
-            <input ref={inpRef} type="text" name='title' className='w-[80%] bg-white h-12 rounded-xl outline-none px-5 text-black text-lg' />
+        <form onSubmit={Submit} className='flex justify-between items-center w-[800px] mt-10  ' >
+            <input ref={inpRef} type="text" name='title' className='w-[80%] bg-white h-12 rounded-xl outline-none px-5 text-black text-lg ' onChange={(e) => settodo(e.target.value)} value={todo} />
             <button type='submit' disabled={loader} className='w-[130px] py-2 cursor-pointer active:opacity-80 rounded-xl bg-black '> {loader ? <div className='w-[30px] h-[30px] border-b-2 border-l-2 rounded-full mx-auto animate-spin ' ></div> : "Create"} </button>
         </form>
     )
